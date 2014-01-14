91eb6bc1b069be4c35aa0d05cfc023477ef62caa
Oli Larkin <olilarkin@googlemail.com>
2014-01-14 12:16:26 +0000

    header header text

--- wsserver.h
+++ wsserver.h
@@ -8,7 +8,7 @@
   
   -- licence --
  
-  Copyright (c) 2014 Oliver Larkin
+  ol.wsserver Copyright (c) 2014 Oliver Larkin
   
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
